8098cfc78c379153822db997b139dd4f32241205
BoKyoungChoi <bokyoung9693@gmail.com>
2025-01-27 23:11:42 +0900

    ADJ: Adjust fetcher to activate merge by ticker

---
+++ FundingRateFetcher/main.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from functools import reduce
+
+from pipeline import PipelineManager
+
+# TODO: Merge datas based on pipeline.py filters, cols by 'ticker'.
+
+
+class Main:
+    def __init__(self,
+                 exch_name: str):
+        pass
+
+
+if __name__ == "__main__":
+    main = Main()
